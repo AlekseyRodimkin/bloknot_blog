390b37d5061cb820c5d35106fdb214bf15a14d79
Rodmkin <alexeyrodimkin@gmail.com>
2024-07-26 23:46:02 +0300

    Added registration and login

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,42 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, EqualTo, Email
+from app.models import User
 
 
 class LoginForm(FlaskForm):
+    """Authorization Form class"""
     username = StringField('Имя', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    """Registration form class"""
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        """
+        The function of checking an existing name
+        :param username: username
+        :return: not or error
+        """
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        """
+        The function of checking an existing email
+        :param email: email address
+        :return: not or error
+        """
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/models.py
+++ app/models.py
@@ -1,23 +1,61 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    """User model"""
     id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')  # one to many to Post
 
     def __repr__(self):
+        """
+        The function returns an unambiguous representation of the users as a string
+        :return: str
+        """
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        """
+        Password replacement function
+        :param password: user password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """
+        Password verification function
+        :param password: user password
+        :return: boolean
+        """
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
+    """Post model"""
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # many to one to Users
 
     def __repr__(self):
+        """
+        The function returns an unambiguous representation of the posts as a string
+        :return: str
+        """
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    """
+    The function will configure the user loader function,
+    which can be called to load a user with an ID
+    :param id: user ID
+    """
+    return User.query.get(int(id))
